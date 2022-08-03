0311ef91d64271fe0dc146398bd66c0ef00ef35a
jerenteria <renteria.juan96@gmail.com>
2022-08-02 20:22:20 -0700

    trying to render the total amount

--- ecommerce/ecommerce_app/urls.py
+++ ecommerce/ecommerce_app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('purchase/<int:product_id>', views.process_purchase),
 ]
--- ecommerce/ecommerce_app/views.py
+++ ecommerce/ecommerce_app/views.py
@@ -10,9 +10,30 @@ def index(request):
     }
     return render(request, "index.html", context)
 
-def render_products(request):
+# def cart(request):
+#     items = []
+#     context = {
+
+#     }
+#     return render(request, "cart.html", context)
+
+def process_purchase(request, product_id):
+    if(request.method == 'POST'):
+        product = Product.objects.get(id=product_id)
+        new_order = Order.objects.create(quantity=int(request.POST['quantity']), total_charge=int(request.POST['quantity']*product.price))
+        new_order.items_ordered.add(product)
+        return render (request, "cart.html")
+    return redirect("/")
+
+
+def render_total(request):
+    all_orders = Order.objects.all()
+    total_spent = 0
+    for order in all_orders:
+        total_spent += order.total_charge
     context = {
-        'all_products': Product.objects.all(),
-        'quantity': [1,2,3,4,5,6,7,8,9,10]
-    }
-    return render(request, "index.html", context)
+            'last_order': Order.objects.last(),
+            'all_orders': all_orders,
+            'grand_total': total_spent,
+        }
+    return render(request, "cart.html", context)
