94571d93586564010ffc28984b1da4b7951a0b34
jerenteria <renteria.juan96@gmail.com>
2022-08-15 19:35:02 -0700

    starting to add stripe to page

--- ecommerce/ecommerce_app/urls.py
+++ ecommerce/ecommerce_app/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('', views.home),
     path('checkout', views.checkout),
     path('<int:product_id>/product', views.one_item),
-    path('<int:product_id>/add-to-cart', views.try_cart)
+    path('<int:product_id>/add-to-cart', views.try_cart),
+    path('render_total', views.checkout),
+    path('pay', views.checkout),
 ]
--- ecommerce/ecommerce_app/views.py
+++ ecommerce/ecommerce_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import ListView, DetailView
 from django.utils import timezone
 from .models import *
+import stripe
 
 # renders all items
 # class based view is easier to create reusable components
@@ -21,6 +22,8 @@ from .models import *
 #     template_name = "product.html"
 
 
+stripe.api_key = 'sk_test_51K2mNoIuO4V9XiaIb1QuwuZFfL66qeCCpOfmp8F0WzHV72gXpKTrcthFIVAXcXb374ox6OvMlZ2esP0YCJtHlj7c00MjtJnz1w'
+
 def home(request):
     context = {
         'all_products': Product.objects.all(),
@@ -34,9 +37,13 @@ def try_cart(request, product_id):
         product = Product.objects.get(id=product_id)
         new_order = Order.objects.create(quantity=int(request.POST['quantity']), total=int(request.POST['quantity'])*product.price)
         new_order.items_ordered.add(product)
-    return redirect('/checkout')
+    return redirect('/checkout') # renders wherever function checkout does
 
-def render_total(request):
+# def charge(request):
+#     if request.method == 'POST':
+
+
+def checkout(request):
     all_orders = Order.objects.all()
     total_spent = 0
     for order in all_orders:
@@ -47,7 +54,6 @@ def render_total(request):
         'grand_total': total_spent,
     }
     return render(request, "cart.html", context)
-
 
 # def add_to_cart(request):
 #     # item = get_object_or_404(Item) #slug=slug) # check to see if user has item or not
@@ -65,8 +71,6 @@ def render_total(request):
 #         order.item.add(order_item)
 #     return redirect('/index')
 
-def checkout(request):
-    return render(request, "cart.html")
 
 def one_item(request, product_id):
     one_item = Product.objects.get(id=product_id)
